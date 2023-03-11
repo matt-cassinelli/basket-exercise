033fb5872fab9e6875ee537a3b7516e1c2aeda1c
matt-cassinelli <mdcassinelli@outlook.com>
2023-03-11 04:27:23 +0000

    Implement adding of products

--- CheckoutKata.Tests/BasketTests.cs
+++ CheckoutKata.Tests/BasketTests.cs
@@ -1,6 +1,4 @@
-using FluentAssertions;
-
-namespace CheckoutKata;
+namespace CheckoutKata.Tests;
 
 public class BasketTests {
 
@@ -14,27 +12,33 @@ public class BasketTests {
     public void When_Construct_Then_Total_Is_Zero()
     {
         var basket = new Basket();
-        basket.Total.ShouldBe(0);
+        basket.Total.Should().Be(0);
     }
 
     [Fact]
     public void When_Construct_Then_Is_Empty()
     {
         var basket = new Basket();
-        basket.Contents.ShouldBeEmpty();
+        basket.Contents.Should().BeEmpty();
     }
 
     [Fact]
-    public void Can_Call_Add()
-    {
-        var basket = new Basket();
-        basket.Add();
-    }
-
-    [Fact]
-    public void Given_Missing_A_Product_When_Call_Add_Then_Throw_ArgumentNullException() {
+    public void Given_Missing_Product_When_Call_Add_Then_Throw_ArgumentNullException() {
         var basket = new Basket();
         Action add = () => basket.Add(null);
-        add.Should().ThrowExactly<ArgumentNullException>();
+        add.Should().Throw<ArgumentNullException>();
+    }
+
+    [Theory]
+    [InlineData(1)]
+    [InlineData(20)]
+    public void Given_A_Product_When_Call_Add_Then_Quantity_Should_Increment(int amountOfProducts) {
+        var basket = new Basket();
+
+        for (int i = 0; i < amountOfProducts; i++) {
+            basket.Add(new Product());
+        }
+        
+        basket.Quantity.Should().Be(amountOfProducts);
     }
 }
--- CheckoutKata/Basket.cs
+++ CheckoutKata/Basket.cs
@@ -1,5 +1,18 @@
 ﻿namespace CheckoutKata;
 
 public class Basket {
+    public decimal Total => 0;
+    public List<Product> Contents = new List<Product>();
+    public int Quantity => Contents.Count();
+
+    public void Add(Product product) {
+        if (product != null)
+            Contents.Add(product);
+        else
+            throw new ArgumentNullException();
+    }
+}
+
+public class Product {
 
 }
