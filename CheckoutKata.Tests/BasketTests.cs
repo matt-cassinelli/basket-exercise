4b8c7c82a5de46819e8aa40c167811b759e6436d
matt-cassinelli <mdcassinelli@outlook.com>
2023-03-11 04:05:50 +0000

    Write some failing tests for Basket

--- CheckoutKata.Tests/BasketTests.cs
+++ CheckoutKata.Tests/BasketTests.cs
@@ -1,10 +1,40 @@
+using FluentAssertions;
+
 namespace CheckoutKata;
 
-public class BasketTests
-{
+public class BasketTests {
+
     [Fact]
-    public void Can_Construct_Basket()
+    public void Can_Construct()
     {
         var basket = new Basket();
+    }
+
+    [Fact]
+    public void When_Construct_Then_Total_Is_Zero()
+    {
+        var basket = new Basket();
+        basket.Total.ShouldBe(0);
+    }
+
+    [Fact]
+    public void When_Construct_Then_Is_Empty()
+    {
+        var basket = new Basket();
+        basket.Contents.ShouldBeEmpty();
+    }
+
+    [Fact]
+    public void Can_Call_Add()
+    {
+        var basket = new Basket();
+        basket.Add();
+    }
+
+    [Fact]
+    public void Given_Missing_A_Product_When_Call_Add_Then_Throw_ArgumentNullException() {
+        var basket = new Basket();
+        Action add = () => basket.Add(null);
+        add.Should().ThrowExactly<ArgumentNullException>();
     }
 }
