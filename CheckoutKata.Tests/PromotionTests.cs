8dcbaf86921de524c828c8c86377c44949fbe1f5
matt-cassinelli <mdcassinelli@outlook.com>
2023-03-12 22:30:33 +0000

    Write red tests for promo 2

--- CheckoutKata.Tests/PromotionTests.cs
+++ CheckoutKata.Tests/PromotionTests.cs
@@ -36,4 +36,25 @@ public class PromotionTests
         basket.Total.Should().Be(55m);
     }
 
+    [Fact]
+    public void When_2_Of_Product_D_Then_Total_Returns_82_5()
+    {
+        var basket = new Basket();
+        basket.Add(new Product("D", "Desk Chair", 55m));
+        basket.Add(new Product("D", "Desk Chair", 55m));
+
+        basket.Total.Should().Be(82.5m);
+    }
+
+    [Fact]
+    public void When_3_Of_Product_D_Then_Total_Returns_137_5()
+    {
+        var basket = new Basket();
+        basket.Add(new Product("D", "Desk Chair", 55m));
+        basket.Add(new Product("D", "Desk Chair", 55m));
+        basket.Add(new Product("D", "Desk Chair", 55m));
+
+        basket.Total.Should().Be(137.5m);
+    }
+
 }
