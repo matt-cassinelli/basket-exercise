4e94a3eae1039c38e952a3e93dabfb35e8d18d8a
matt-cassinelli <mdcassinelli@outlook.com>
2023-03-12 21:55:48 +0000

    Implement promo 1

---
+++ CheckoutKata/Promotion.cs
@@ -0,0 +1,15 @@
+﻿namespace CheckoutKata;
+
+public class Promotion
+{
+    public Promotion(string name, string forSku, decimal discount)
+    {
+        Name = name;
+        ForSku = forSku;
+        Discount = discount;
+    }
+
+    public string Name { get; }
+    public string ForSku { get; }
+    public decimal Discount { get; }
+}
